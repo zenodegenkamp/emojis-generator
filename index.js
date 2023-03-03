3d31fde9b13dafb89744c09adf87dbd6b222f2a1
zenode <46578099+zenodegenkamp@users.noreply.github.com>
2023-03-03 12:20:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const myEmojis = ["🌎", "🏋", "💻"]
+const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
+
+function renderEmojis() {
+    emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
+}
+
+renderEmojis()
+
+
+unshiftBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+popBtn.addEventListener("click", function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
